8914a456a588c7a369a43517d5fff59ed625295a
tosiki1202-afk <tosiki0002@gmail.com>
2025-01-28 02:39:42 +0900

    lasthouse

--- Assets/Scripts/MSAvatorAjuster.cs
+++ Assets/Scripts/MSAvatorAjuster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AvatorAjuster : MonoBehaviour
+public class MSAvatorAjuster : MonoBehaviour
 {
     public GameObject Camera;
     public GameObject Avator;
@@ -20,11 +20,11 @@ public class AvatorAjuster : MonoBehaviour
                                             Camera.transform.localPosition.z);
             
             // 1.66=vinci直立時のheadの位置
-            Avator.transform.localScale = new Vector3(Avator.transform.localScale.x*Camera.transform.localPosition.y/1.624f,
-                                                      Avator.transform.localScale.y*Camera.transform.localPosition.y/1.624f,
+            Avator.transform.localScale = new Vector3(Avator.transform.localScale.x*Camera.transform.localPosition.y/1.58f,
+                                                      Avator.transform.localScale.y*Camera.transform.localPosition.y/1.58f,
                                                       Avator.transform.localScale.z);
 
-            Debug.Log("アバタースケール:"+Camera.transform.localPosition.y/1.624f);
+            Debug.Log("アバタースケール:"+Camera.transform.localPosition.y/1.58f);
             Debug.Log("修正後のディスプレイの高さ:"+Camera.transform.localPosition.y);
         }
     }
